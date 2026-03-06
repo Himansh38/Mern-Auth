ae3c955416c6d46ce7349a78596736c86654834e
Himanshu Gupta <138051620+Himansh38@users.noreply.github.com>
2026-03-06 23:41:16 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -1,48 +1,26 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
-import cookieParser from "cookie-parser";
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 import connectDB from "./config/mongodb.js";
-import authRouter from "./routes/authRouters.js";
+import authRouter from "./routes/authRouters.js"
 import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
-
-// Connect Database
 connectDB();
 
-// Allowed Origins
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://mern-auth-delta-six.vercel.app"
-];
+const allowedOrigins = ['http://localhost:5173','https://mern-auth-delta-six.vercel.app']
 
-// CORS Middleware
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  })
-);
-
-// Middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin : allowedOrigins,credentials : true}))
 
-// API Endpoints
-app.get("/", (req, res) => res.send("Server is running"));
+//API Endpoints 
+app.get('/', (req, res)=> res.send("server is running"))
+app.use('/api/auth' , authRouter)
+app.use('/api/user' , userRouter)
 
-app.use("/api/auth", authRouter);
-app.use("/api/user", userRouter);
 
-// Start Server
-app.listen(port, () => {
-  console.log(`Server started on PORT: ${port}`);
-});
+app.listen(port , ()=> console.log(`server  started on PORT : ${port}`))
+
