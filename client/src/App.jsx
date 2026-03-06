f66cd0895821e5c8e07a54ad86ee097e74e380f8
Himanshu Gupta <138051620+Himansh38@users.noreply.github.com>
2026-03-06 16:10:43 +0530

    full stack Authentication app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <Routes>
         <Route path='/' element= {<Home/>} />
         <Route path='/login' element= {<Login/>} />
-        <Route path='/email-verify' element= {<EmailVerify/>} />
+        <Route path='/verify-account' element= {<EmailVerify/>} />
         <Route path='/reset-password' element= {<ResetPassword />} />
       </Routes>
     </div>
